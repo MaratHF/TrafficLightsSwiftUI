a8da0cf5804b93e44cdbc9fd4c2157759220dd20
Marat Khayrullin <m.f.khayrullin@gmail.com>
2022-08-11 23:16:10 +0300

    Initial Commit

---
+++ TrafficLightsSwiftUI/ChangeColorButton.swift
@@ -0,0 +1,36 @@
+//
+//  ChangeColorButton.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by MAC on 22.07.2020.
+//
+
+import SwiftUI
+
+struct ChangeColorButton: View {
+    let title: String
+    let action: () -> Void
+
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.white)
+
+        }
+        .frame(width: 200, height: 60)
+        .background(Color(.blue))
+        .cornerRadius(20)
+        .overlay(
+            RoundedRectangle(cornerRadius: 20, style: .continuous)
+                .stroke(Color.white, lineWidth: 4)
+        )
+    }
+}
+
+struct ChangeColorButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ChangeColorButton(title: "START", action: {})
+    }
+}
---
+++ TrafficLightsSwiftUI/TrafficLightsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsSwiftUIApp.swift
+//  TrafficLightsSwiftUI
+//
+//  Created by MAC  on 12.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
